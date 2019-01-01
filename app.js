817442d8f52b396c3b881c91ebb54b5466f80cc8
LAPTOP-FQO0R4FD\HP <teriologia807@gmail.com>
2019-01-02 02:34:44 +0300

    variables test

---
+++ app.js
@@ -0,0 +1,13 @@
+var data = 2;
+
+var stringvar = "deneme string";
+
+var cokluDegiskenTanımlama = "denem değişkenş",
+    ikinciDegisken = 12,
+    ucuncuDegisken = "yes";
+
+console.log(data);
+console.log(stringvar);
+console.log(cokluDegiskenTanımlama);
+console.log(ikinciDegisken);
+console.log(ucuncuDegisken);
