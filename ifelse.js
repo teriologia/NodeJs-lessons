c03e52d9c43ae7ceb8f108dcab503499375c65da
LAPTOP-FQO0R4FD\HP <teriologia807@gmail.com>
2019-01-02 17:15:30 +0300

    İf else in node js

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+var hungry = false;
+var thirsty = true;
+
+if(hungry){
+    console.log("yemek ye");
+}
+else if (thirsty){
+    console.log("su iç");
+}
+else {
+    console.log("yemek yeme");
+}
