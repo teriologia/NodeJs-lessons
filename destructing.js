27df6250f4613b8267ec835c2689cf9cd73aa972
LAPTOP-FQO0R4FD\HP <teriologia807@gmail.com>
2019-01-05 12:04:03 +0300

    destructing-2

--- destructing.js
+++ destructing.js
@@ -1,11 +1,11 @@
 const degerler = {
     deger1: "değer1",
     deger2: "değer2",
-    deger3: "değer3"
+    deger3: {isim:"serdar"}
 };
 
-const {deger1, deger2, deger4 = "değer4"} = degerler;
-console.log(deger1, deger2, deger4);
+const {deger1, deger2, deger4 = "değer4", deger3: {isim}} = degerler;
+console.log(deger1, deger2, deger4,isim);
 
 const dizi = [
     1,2,3
