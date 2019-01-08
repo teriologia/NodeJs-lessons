b70faf04eba0949080c48f863abe257bd7e3ffc7
LAPTOP-FQO0R4FD\HP <teriologia807@gmail.com>
2019-01-08 12:21:40 +0300

    PUG Layout block

--- Express/expressFirst.js
+++ Express/expressFirst.js
@@ -6,5 +6,13 @@ app.set('view engine', 'pug');
 app.get('/', (req,res) =>{
     res.render('index.pug', {name: 'serdar ', surname: 'tekin '});
 });
+app.get('/home', (req,res) =>{
+    res.render('home.pug');
+});
+app.get('/contact', (req,res) =>{
+    res.render('contact.pug');
+});
+
+
 
 app.listen(3000);
