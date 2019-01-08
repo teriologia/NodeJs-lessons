da9857cb0b6345d48449efb495a386d34aca21e1
LAPTOP-FQO0R4FD\HP <teriologia807@gmail.com>
2019-01-08 11:35:44 +0300

    first pug page

--- Express/expressFirst.js
+++ Express/expressFirst.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req,res) =>{
-    res.send('merhaba express');
+    res.render('index.pug');
 });
 
 app.listen(3000);
