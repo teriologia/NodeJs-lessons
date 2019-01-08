0bbcfb58d1f1a64311155bd84cfbecf5a1620eb0
LAPTOP-FQO0R4FD\HP <teriologia807@gmail.com>
2019-01-08 11:02:21 +0300

    Write Steam

--- node-http/readstream.js
+++ node-http/readstream.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const file = '1111.mp4';
 
 const readStream  = fs.createReadStream(file);
+const writeStream = fs.createWriteStream('deneme.mp4');
+
 let progress = 0;
 fs.stat(file, (err,data) => {
     const total = data.size;
@@ -11,9 +13,8 @@ readStream.on('data', (chunk) => {
     progress+=chunk.length;
     console.log(Math.round((100*progress)/total)+'%');
     });
-
-readStream.on('end', () => {
-    console.log('veri bitti');
-    //console.log(progress);
-    });
+    readStream.pipe(writeStream);
+    writeStream.on('finish',()=>{
+        console.log('yeni dosya oluşturuldu');
+    })
 });
